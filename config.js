af530a38037e333bdd9f212e51a4244734d0d4ae
m1000vrm <mv069411@gmail.com>
2026-09-07 15:04:19 -0300

    Update checkoutUrl in BIOFY_CONFIG

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.BIOFY_CONFIG = Object.freeze({
-  checkoutUrl: 'https://pay.kirvano.com/r/bb5f3381-5253-4125-993c-b36b6221020b'
+  checkoutUrl: 'https://pay.kirvano.com/6d6b96d1-c159-4105-80c6-e223d74195ef?aff=bb5f3381-5253-4125-993c-b36b6221020bb'
 });
